b8c6e0fe399284183afcbcf9df5ac565e9a3952d
Scisco <scisco7@gmail.com>
2015-11-25 11:01:05 -0500

    first commit

---
+++ README.rst
@@ -0,0 +1,38 @@
+geojson-area
+============
+
+.. image:: https://travis-ci.org/scisco/boolean.svg?branch=develop
+    :target: https://travis-ci.org/scisco/boolean
+
+.. image:: https://badge.fury.io/py/boolean.svg
+    :target: http://badge.fury.io/py/boolean
+
+.. image:: https://img.shields.io/pypi/l/boolean.svg
+    :target: https://pypi.python.org/pypi/boolean/
+    :alt: License
+
+
+Calculate the area inside of any [GeoJSON](http://geojson.org/) geometry. This is a port of Mapbox's [geojson-area](https://github.com/mapbox/geojson-area) for Python.
+
+Installation
+------------
+
+.. code::
+
+  $ pip install area
+
+Usage
+-----
+
+.. code::
+
+  >>> from area import area
+  >>> obj = {'type':'Polygon','coordinates':[[[-180,-90],[-180,90],[180,90],[180,-90],[-180,-90]]]}
+  >>> area(obj)
+  511207893395811.06
+
+
+Credit
+------
+
+- [geojson-area](https://github.com/mapbox/geojson-area)
---
+++ test.py
@@ -0,0 +1,51 @@
+import json
+import unittest
+
+from area import area
+
+
+f = open('illinois.json', 'U')
+illinois = json.loads(f.read())
+
+world = {
+    'type': 'Polygon',
+    'coordinates': [
+        [
+            [-180, -90],
+            [-180, 90],
+            [180, 90],
+            [180, -90],
+            [-180, -90]
+        ]
+    ]
+}
+
+illinois_area = 145978332359.36716
+world_area = 511207893395811.06
+
+class AreaTestCase(unittest.TestCase):
+
+    def test_area_illinois(self):
+        """ Compute the area of illinois """
+        self.assertEqual(area(illinois), illinois_area)
+
+    def test_area_world(self):
+        """ Compute the area of the whole world """
+        self.assertEqual(area(world), world_area)
+
+    def test_point_area(self):
+        """ Compute the area of a point """
+        self.assertEqual(area({'type':'Point', 'coordinates': [0,0]}), 0)
+
+    def test_liststring_area(self):
+        """ Compute the area of a line string """
+        self.assertEqual(area({'type':'LineString', 'coordinates': [[0,0],[1,1]]}), 0)
+
+    def test_geometry_collection_area(self):
+        """ Compute the area of a geometry collection """
+        total = illinois_area + world_area
+        self.assertEqual(area({'type': 'GeometryCollection', 'geometries': [world, illinois]}), total)
+
+
+if __name__ == '__main__':
+    unittest.main()
